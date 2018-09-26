34422c38ea9775d01e3b7fbc8c19e514659919f2
Hai Phung <hphung@worldventures.com>
2018-09-26 10:02:04 -0500

    first commit

---
+++ SuperDisco/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperDisco
+//
+//  Created by Hai Phung on 9/26/18.
+//  Copyright © 2018 WorldVentures. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
